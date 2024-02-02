75ae52cc4075576105be8aee1c1a90151c0c4210
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-01 22:20:27 -0600

    Linking Between Routes With <Link /> and <NavLink />

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,11 @@
+import PageNav from "../components/PageNav";
+
 function PageNotFound() {
-  return <div>Not found :(</div>;
+  return (
+    <div>
+      <PageNav /> Not found :(
+    </div>
+  );
 }
 
 export default PageNotFound;
