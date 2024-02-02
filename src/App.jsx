a250f0fe4f3fd8d14b6b2706275b5f79aa7d8be1
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-01 20:19:38 -0600

    Implementing Main Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+
+//////////////////////////////////////////////
+// Creating Our First App With Vite: WorldWise
 function App() {
-  return <div>WorldWise</div>;
+  /////////////////////////////////////
+  // Implementing Main Pages and Routes
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
