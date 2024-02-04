42847aa90af72bc9cb13d9b09a53dd343a0a8ea7
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-04 15:59:44 -0600

    Programmatic Navigation with <Navigate />

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
@@ -40,8 +40,10 @@ function App() {
   // Implementing Main Pages and Routes
   ////////////////////////////////
   // Nested Routes and Index Route
-  ////////////////////////////////
+  /////////////////////////////////////
   // Dynamic Routes With URL Parameters
+  ////////////////////////////////////////////
+  // Programmatic Navigation with <Navigate />
   return (
     <BrowserRouter>
       <Routes>
@@ -50,10 +52,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
